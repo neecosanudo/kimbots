e55fa9760aef5f3aa1eca7a05479306898edae05
Nicolás Sañudo <hola@neecosanudo.com>
2022-08-18 00:36:47 -0300

    💡 Docs: agrego documentación en código.
    - CalculadoraDto
    - Calculadora.dividir

--- src/index.dto.ts
+++ src/index.dto.ts
@@ -1,3 +1,9 @@
+/**
+ *  Interface para métodos de una calculadora.
+ *
+ *  @privateRemarks Los métodos que contiene son:
+ *  - dividir
+ */
 export default interface CalculadoraDto {
   dividir: (numerador: number, denominador: number) => number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import CalculadoraDto from './index.dto';
 
 export default class Calculadora implements CalculadoraDto {
+  /**
+   * Divide dos números entre si y devuelve el resultado.
+   * Si el denominador es cero, devuelve cero.
+   *
+   * @param numerador - Cualquier número.
+   * @param denominador - Cualquier número.
+   * @returns Resultado de dividir numerador por denominador.
+   */
   dividir(numerador: number, denominador: number): number {
+    /** Comprueba que el denominador no sea cero */
     if (denominador === 0) {
       return 0;
     }
+
     const resultado = numerador / denominador;
     return resultado;
   }
